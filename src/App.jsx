2d911aee492b4e6a25c4d1b70a63240f8aae9f8e
Хижняк Петро Петрович <khyzhiak-pp@utg.ua>
2025-05-28 09:38:23 +0300

    first task is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
+import { useState, createElement, Fragment } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    /*<>
+        <h1>Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid asperiores consectetur, deserunt distinctio dolor, fugit id, magni necessitatibus neque officiis quos repellendus repudiandae sapiente ullam velit voluptate. Quisquam, voluptas.</p>
+        <img src={reactLogo} alt="react logo"/>
+    </>*/
+      createElement(
+          Fragment,
+          null,
+            createElement('h1', null, 'Header'),
+            createElement('p', null, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid asperiores consectetur, deserunt distinctio dolor, fugit id, magni necessitatibus neque officiis quos repellendus repudiandae sapiente ullam velit voluptate. Quisquam, voluptas.'),
+            createElement('img', { src: reactLogo, alt: 'react logo' }),
+      )
   )
 }
 
